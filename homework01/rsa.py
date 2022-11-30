a057992ceb5c844dab59117ad06ec9b9ecb4cdec
alexandrov45 <alexandroffs45@gmail.com>
2022-12-01 00:53:53 +0300

    pls

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -12,9 +12,9 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    if n < 2 or n % 2 == 0:
+    if n < 2:
         return False
-    for i in range(3, int(n**0.5) + 1, 2):
+    for i in range(2, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
