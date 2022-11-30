b1e6b308f91187ca71442f9e1282c3f521fea5a9
alexandrov45 <alexandroffs45@gmail.com>
2022-12-01 01:03:32 +0300

    YA HO4U SPAT

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -14,6 +14,8 @@ def is_prime(n: int) -> bool:
     """
     if n < 2:
         return False
+    if n == 2:
+        return True 
     if n % 2 == 0:
         return False
     for i in range(3, int(n**0.5) + 1, 2):
